0c11e4f1af4f7431a7d64121b9f4080e69fb6461
liuqi <13761299894@163.com>
2022-06-14 17:45:56 +0800

    3.5.9_条件操作符

---
+++ src/unit3/3.5_操作符/3.5.9_条件操作符.js
@@ -0,0 +1,13 @@
+// 3.5.9 条件操作符
+/**
+ * 条件操作符是ECMAScript中最广泛的操作符之一，语法和Java中一样：
+ * val = boolean_expression ? true_value : false_value;
+ */
+
+(() => {
+    function compareVal(num1, num2) {
+        return num1 > num2 ? num1 : num2;
+    }
+    console.log(compareVal(2, 3));  // 3
+    console.log(compareVal(3, 2));  // 3
+})();
